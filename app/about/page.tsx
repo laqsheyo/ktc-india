a374d770d763c6958ce772eca771445ec5019803
Laqshay Arora <laqsheyo@gmail.com>
2026-04-04 17:34:29 +0530

    updated contact page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,6 @@
 export default function About() {
   return (
     <main className="about-page">
-
-      {/* TITLE */}
-      <section className="center section">
-        <img src="/images/1.jpg" alt="Office" />
-      </section>
-
       {/* ABOUT */}
       <section className="center section">
         <img src="/images/2.jpg" alt="About" />
@@ -26,12 +20,6 @@ export default function About() {
       <section className="center section">
         <img src="/images/5.jpg" alt="Services" />
       </section>
-
-      {/* BRAND */}
-      <section className="center section">
-        <img src="/images/6.jpg" alt="Brand" />
-      </section>
-
       {/* CERTIFICATIONS */}
       <section className="center section">
         <img src="/images/7.jpg" alt="Certificates" />
