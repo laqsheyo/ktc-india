422e63bcc56dd3c3b66edd9ac9b0f9ac3ffc5c60
Laqshay Arora <laqsheyo@gmail.com>
2026-04-06 16:38:01 +0530

    updated contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,18 +5,46 @@ export default function Contact() {
       <h1>KTC India Display LLP</h1>
 
       <p>
-        707-710 Paras Trinity, Sector 63, Gurugram<br />
+        707-710 Paras Trinity, Maidawas Rd, Sector 63,<br />
+        Gurugram, Haryana 122098<br />
         Phone: <a href="tel:+917683005899">+91 76830 05899</a>
       </p>
 
+      {/* GOOGLE MAP */}
       <div className="map-container">
         <iframe
           src="https://www.google.com/maps?q=707-710%20Paras%20Trinity%20Sector%2063%20Gurugram&output=embed"
           width="100%"
           height="350"
           style={{ border: 0 }}
+          loading="lazy"
         ></iframe>
       </div>
+
+      {/* DIRECTIONS BUTTON */}
+      <a
+        href="https://maps.app.goo.gl/UXbdc5HDw2rSrYeg8?g_st=ac"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="directions-btn"
+      >
+        Get Directions
+      </a>
+
+      {/* CONTACT FORM (RESTORED) */}
+      <form
+        action="https://formsubmit.co/laqsheyo@gmail.com"
+        method="POST"
+        className="contact-form"
+      >
+        <input type="text" name="name" placeholder="Your Name" required />
+
+        <input type="email" name="email" placeholder="Your Email" required />
+
+        <textarea name="message" placeholder="Your Message" required />
+
+        <button type="submit">Send Message</button>
+      </form>
 
     </main>
   );
