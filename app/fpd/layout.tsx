b4c6d630a347c6a6564f979db011dc71fedcffc1
Laqshay Arora <laqsheyo@gmail.com>
2026-05-08 13:41:14 +0530

    updated contact page

--- app/fpd/layout.tsx
+++ app/fpd/layout.tsx
@@ -1,8 +1,17 @@
-export default function FPDLayout({ children }: { children: React.ReactNode }) {
+// app/fpd/layout.tsx
+import "../globals.css";
+
+export default function FPDLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div>
-      {/* NO main site header here */}
-      {children}
-    </div>
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0, background: "#000" }}>
+        {/* NO KTC header here - just children */}
+        {children}
+      </body>
+    </html>
   );
 }
