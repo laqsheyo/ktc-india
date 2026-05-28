65e72fb4b49366f0c85a7adb98948e0a2e73f280
Laqshay Arora <laqsheyo@gmail.com>
2026-05-28 13:54:12 +0530

    updated contact page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  const [active, setActive] = useState<string | null>(null);
 
   const handleClick = (brand: string) => {
-    setActive(brand);
-
-    setTimeout(() => {
-      if (brand === "FPD") {
-        router.push("/fpd");
-      } else if (brand === "KTC") {
-        router.push("/ktc");
-      } else {
-        router.push("/brands");
-      }
-    }, 800);
+    if (brand === "FPD") {
+      router.push("/fpd");
+    } else if (brand === "KTC") {
+      router.push("/ktc");
+    } else {
+      router.push("/brands");
+    }
   };
 
   return (
@@ -33,34 +27,32 @@ export default function Home() {
         </div>
 
         <div className="hero-brands">
-          <button className="brand-circle" onClick={() => handleClick("KTC")}>
+          <button
+            className="brand-logo-button"
+            onClick={() => handleClick("KTC")}
+          >
             <img src="/images/ktc.jpg" alt="KTC" />
-            {active === "KTC" && <span className="brand-label">KTC</span>}
-          </button>
-
-          <button className="brand-circle" onClick={() => handleClick("FPD")}>
-            <img src="/images/fpd.jpg" alt="FPD" />
-            {active === "FPD" && <span className="brand-label">FPD</span>}
           </button>
 
           <button
-            className="brand-circle"
+            className="brand-logo-button"
+            onClick={() => handleClick("FPD")}
+          >
+            <img src="/images/fpd.jpg" alt="FPD" />
+          </button>
+
+          <button
+            className="brand-logo-button"
             onClick={() => handleClick("Horion")}
           >
             <img src="/images/horion.png" alt="Horion" />
-            {active === "Horion" && (
-              <span className="brand-label">Horion</span>
-            )}
           </button>
 
           <button
-            className="brand-circle"
+            className="brand-logo-button"
             onClick={() => handleClick("Eclara")}
           >
             <img src="/images/eclara.jpg" alt="Eclara" />
-            {active === "Eclara" && (
-              <span className="brand-label">Eclara</span>
-            )}
           </button>
         </div>
       </section>
